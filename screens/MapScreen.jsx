8b12369f09582e37c6665c17282fa1ea83bf10c9
Jose Luis C <j.carrasco.cortes@gmail.com>
2020-05-29 15:50:26 +0200

    export names fixed

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function ChatScreen() {
+export default function MapScreen() {
   return (
     <View style={styles.container}>
       <Text>MapScreen</Text>
