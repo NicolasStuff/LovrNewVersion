679fd2aa0102b0a69e55ed680ea96b238bdd08e2
NicolasStuff <nicolas.ivorra.ni@gmail.com>
2020-06-01 11:42:59 +0200

    facebookLogin v1 (not functionnal)

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Facebook from 'expo-facebook';
 
 export default function HomeScreen() {
+  async function logIn() {
+    try {
+      await Facebook.initializeAsync('2524688944298263');
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
