45285e40462c140c8350b0c4808a029bdb7bf357
NicolasStuff <nicolas.ivorra.ni@gmail.com>
2020-06-02 16:57:46 +0200

    login screen updated

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './screens/HomeScreen';
 import SettingScreen from './screens/SettingScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import MapScreen from'./screens/MapScreen';
+//import MapScreen from'./screens/MapScreen';
 import ContactsScreen from './screens/ContactsScreen';
 import ChatScreen from './screens/ChatScreen';
 
@@ -21,9 +21,9 @@ const Stack = createStackNavigator();
 function MyStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Map" component={MapScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      {/* <Stack.Screen name="Map" component={MapScreen} /> */}
+      <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Settings" component={SettingScreen} />
       <Stack.Screen name="Contacts" component={ContactsScreen} />
       <Stack.Screen name="Chat" component={ChatScreen} />
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function MapScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>MapScreen</Text>
-      <Button
-        title="Go to Contacts"
-        onPress={() => navigation.navigate('Contacts')}
-      />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+    <View>
+      <TouchableOpacity onPress={() => this.props.navigation.navigate('ProfileScreen')} style={styles.profileLink}>
+        <Image source={require('../img/ProfileLogoFromMaps.png')} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.props.navigation.navigate('ContactsScreen')} style={styles.ChatLink}>
+        <Image source={require('../img/ConversationLogoFromMap.png')} />
+      </TouchableOpacity>
+      <MapView style={{flex : 1}}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
     </View>
   );
 }
