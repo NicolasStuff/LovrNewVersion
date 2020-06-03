04cf2127482bc125346c97eb9b5e05ffd1013a15
NicolasStuff <nicolas.ivorra.ni@gmail.com>
2020-06-03 12:36:46 +0200

    map fix n2

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity ,Dimensions, mapCustom} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
