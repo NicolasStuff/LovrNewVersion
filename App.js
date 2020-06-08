8c6ab6d8df90a5303e95dee1ef208bf11bba2ef3
NicolasStuff <nicolas.ivorra.ni@gmail.com>
2020-06-08 15:09:19 +0200

    part 1 chat Screen

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 /* Hide Error Messages */ 
 console.disableYellowBox = true;
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import { Button } from 'react-native';
+import { Input } from 'react-native-elements';
+
 
 /* Navigation Between Screens */
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 /* Screens */
 import HomeScreen from './screens/HomeScreen';
@@ -14,6 +18,8 @@ import ProfileScreen from './screens/ProfileScreen';
 import MyProfileScreen from './screens/MyProfileScreen';
 import MapScreen from'./screens/MapScreen';
 import ContactsScreen from './screens/ContactsScreen';
+import RequestScreen from './screens/RequestScreen';
+
 import EditProfile from './screens/EditProfileScreen';
 import ChatScreen from './screens/ChatScreen';
 
@@ -25,28 +31,53 @@ const store = createStore(combineReducers({ user }));
 
 /* createStackNavigator() */
 const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  
+  return (
+    <Tab.Navigator tabBarOptions= {{labelStyle: { textTransform: 'none' }}}>
+      <Tab.Screen name="Conversation" component={ChatScreen} />
+      <Tab.Screen name="Demande de chat" component={RequestScreen} />
+    </Tab.Navigator>
+  );
+}
 
 function MyStack() {
+
+  const [text, setText] = useState()
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="EditProfile" component={EditProfile} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="MyProfile" component={MyProfileScreen} />
-      <Stack.Screen name="Map" component={MapScreen} />
-      <Stack.Screen name="Settings" component={SettingScreen} />
-      <Stack.Screen name="Contacts" component={ContactsScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Map" component={MapScreen} options={{headerShown: false}} />
+        <Stack.Screen name="EditProfile" component={EditProfile} options={{headerShown: false}}/>
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="MyProfile" component={MyProfileScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Settings" component={SettingScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Chat" component={MyTabs} options={{
+              headerTitle: () => (
+              <Input
+                placeholder='Rechercher'
+                placeholderTextColor= 'red'
+                leftIcon={{ type: 'Feather', name: 'search' , color: '#FF3C5E'}}
+                onChangeText={value => setText( value )}
+                style={{borderBottomColor: 'red' }}
+              />
+            ),
+          }}/>
+        {/* <Stack.Screen name="Contacts" component={ContactsScreen} /> */} 
+        {/* <Stack.Screen name="Chat" component={ChatScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 export default function App() {
   return (
     <Provider store={store}>
-    <NavigationContainer>
       <MyStack />
-    </NavigationContainer>
     </Provider>
   );
 }
